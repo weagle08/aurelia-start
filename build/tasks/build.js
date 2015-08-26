c5d6232cff714762127091518db5f76c95b3086b
Ben Johnson <weagle08@gmail.com>
2015-08-25 22:58:17 -0500

    working on build

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -13,8 +13,14 @@ var assign = Object.assign || require('object.assign');
 gulp.task('build-view-models', function(){
     return gulp.src(paths.viewModels)
         .pipe(plumber())
-        .pipe(changed(paths.output, {extension: '.js'}))
+        .pipe(changed(paths.output.viewModels, {extension: '.js'}))
         .pipe(sourcemaps.init({loadMaps: true}))
         .pipe(ecma5(assign({}, compileOptions, {modules: 'system'})))
-        .pipe(gulp.dest(paths.output))
+        .pipe(gulp.dest(paths.output.viewModels));
+});
+
+gulp.task('build-views', function () {
+    return gulp.src(paths.views)
+        .pipe(changed(paths.output.views, {extension: '.html'}))
+        .pipe(gulp.dest(paths.output.views));
 });
