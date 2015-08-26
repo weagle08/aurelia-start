5873696663d724e3fa4298084883945526c04619
ben johnson <weagle08@gmail.com>
2015-08-25 21:04:52 -0500

    meh

---
+++ build/paths.js
@@ -0,0 +1,24 @@
+/**
+ * Created by ben on 8/25/15.
+ */
+var appRoot = 'src/';
+var outRoot = 'dist/';
+
+module.exports = {
+    root: appRoot,
+    models: appRoot + 'models/**/*.js',
+    views: appRoot + 'vvm/**/*.html',
+    viewModels: appRoot + 'vvm/**/*.js',
+    styles: {
+        css: appRoot + 'styles/**/*.css',
+        sass: appRoot + 'styles/**/*.sass'
+    },
+    output: {
+        root: outRoot,
+        viewModels: outRoot + 'vvm/',
+        views: outRoot + 'vvm/',
+        styles: outRoot + 'styles/'
+    },
+    documentation:'./docs',
+    tests: './test/**/*.js'
+};
--- build/tasks/build.js
+++ build/tasks/build.js
@@ -1,3 +1,20 @@
 /**
- * Created by weagl on 8/24/2015.
+ * Created by ben on 8/24/2015.
  */
+var gulp = require('gulp');
+var changed = require('gulp-changed');
+var plumber = require('gulp-plumber');
+var ecma5 = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
+var paths = require('../paths');
+var compileOptions = require('../babel-options');
+var assign = Object.assign || require('object.assign');
+
+gulp.task('build-view-models', function(){
+    return gulp.src(paths.viewModels)
+        .pipe(plumber())
+        .pipe(changed(paths.output, {extension: '.js'}))
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(ecma5(assign({}, compileOptions, {modules: 'system'})))
+        .pipe(gulp.dest(paths.output))
+});
